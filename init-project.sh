a1b2a9d13821cd14c2d95d7195ec5bfcdacff965
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-06 19:00:44 +0100

    sync with skeleton base

--- init-project.sh
+++ init-project.sh
@@ -2,7 +2,7 @@
 # This tool is to initialize the project after cloning.
 # The goal is to make easy to create and test new projects.
 
-CAMEL=$(echo "${1}" | grep -E '^([A-Z][a-z0-9]+){2,}$') || CAMEL=""
+CAMEL=$(echo "${1}" | grep -E '^([A-Z]+[a-z0-9]*){2,}$') || CAMEL=""
 
 if [ "${CAMEL}" = "" ]; then
     echo "Usage: ${0} UpperCamelCaseName"
@@ -21,8 +21,7 @@ else
 fi
 
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
-# TODO: Uncomment the next line after initializing a concrete skeleton project.
-#rm init-project.sh sync-project.sh
+rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/LispSkeleton/${2}/g" -e "s/lisp-skeleton/${3}/g" "${4}"' '_' "${SED}" "${CAMEL}" "${DASH}" '{}' \;
 echo "Done. Files were edited and moved. Review those changes."
--- sync-project.sh
+++ sync-project.sh
@@ -37,8 +37,7 @@ cp ./*.sh "${TARGET_PROJECT}"
 cp .gitignore "${TARGET_PROJECT}"
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 cd "${TARGET_PROJECT}" || exit 1
-# TODO: Uncomment the next line after initializing a concrete skeleton project.
-#rm init-project.sh sync-project.sh
+rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/LispSkeleton/${2}/g" -e "s/lisp-skeleton/${3}/g" "${4}"' '_' "${SED}" "${CAMEL}" "${DASH}" '{}' \;
 echo "Done. Files were copied to ${TARGET_PROJECT} and modified. Review those changes."
